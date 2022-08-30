c8d22e9daa9ed13a4f62bc30de8a52b75a9aa549
Tomoaki Takebuchi <ttakekbuchi@H12968.ad.hugeinc.com>
2022-08-30 12:24:18 +0900

    intro section finished

---
+++ components/Intro/intro.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { useTheme } from "next-themes";
+import { Grid } from "../Foundations/foundations";
+import { IntroContainer, ResumeLink, Title } from "./styles";
+
+export default function Intro() {
+  const { theme } = useTheme();
+  console.log("🚀 ~ file: intro.tsx ~ line 8 ~ Intro ~ theme", theme);
+
+  return (
+    <IntroContainer color={theme}>
+      <Title>
+        Hi, I am a frontend developer working at a web design agency 👋
+      </Title>
+      <h2>Building Web applications with latest technologies</h2>
+      <Link href="/resume" passHref>
+        <ResumeLink>Learn more about my background</ResumeLink>
+      </Link>
+    </IntroContainer>
+  );
+}
