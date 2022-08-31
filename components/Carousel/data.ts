d9a5c623843f57a4f8c6bf73ab591122dd9a6722
Tomoaki Takebuchi <ttakekbuchi@H12968.ad.hugeinc.com>
2022-08-31 23:09:17 +0900

    initial commit for carousel component

---
+++ components/Carousel/data.ts
@@ -0,0 +1,24 @@
+export const CarouselItems = [
+  {
+    title: "MERN-Chat-App",
+    imgURL: "/",
+    description:
+      "Real-time chat app built with MERN stack (full-stack project)",
+    stack: "React, Nodejs, Express, MongoDB, Socketio, Bootstrap",
+    url: "https://mern-realtime-chat-app.netlify.app/",
+  },
+  {
+    title: "Path Finder Visualization",
+    imgURL: "/",
+    description: "A python visualization of the A* path finding algorithm.",
+    stack: "Python",
+    url: "https://github.com/Tomo5524/Path_Finder_Vizualization",
+  },
+  {
+    title: "Rakuten Fashion",
+    imgURL: "/",
+    description: "Developed a high end luxury website",
+    stack: "React, Typescript, Redux, Sass",
+    url: "https://brandavenue.rakuten.co.jp/contents/designers/?l-id=brn_top_all_ldc_des_all",
+  },
+];
