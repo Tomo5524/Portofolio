8ee28286a5dfcf2f6d8364d2f6dd94e6ac3c6543
Tomoaki Takebuchi <ttakekbuchi@H12968.ad.hugeinc.com>
2022-08-31 23:08:34 +0900

    added getLayout function to dynamically render layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,30 @@ import { GlobalStyle } from "../theme/GlobalStyle";
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 import "../styles/loader.css";
+import { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
+import { Router } from "next/router";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement, router: Router) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
 
 // pass in router to pass router down to child component
-function MyApp({ Component, pageProps, router }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppPropsWithLayout) {
+  // some pages don't have layout (container and header)
+  const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <>
       <GlobalStyle />
       <ThemeProvider>
-        <Layout router={router}>
+        {/* <Layout router={router}>
           <Component {...pageProps} />
-        </Layout>
+        </Layout> */}
+        {getLayout(<Component {...pageProps} />, router)}
       </ThemeProvider>
     </>
   );
