c3584c65008ddd2d4f5a8e07bce6c1747d8e6f54
Tomoaki Takebuchi <ttakekbuchi@H12968.ad.hugeinc.com>
2022-09-12 23:32:25 +0900

    created project link for mobile

--- components/Intro/styles.tsx
+++ components/Intro/styles.tsx
@@ -42,50 +42,66 @@ export const Title = styled.div`
 `;
 
 export const ProjectLink = styled.a`
-  text-align: center;
-  --c: #495049; /* the color */
-  --b: 0.1em; /* border length*/
-  --d: 20px; /* the cube depth */
-  --_s: calc(var(--d) + var(--b));
-  color: var(--c);
-  &:focus-visible {
-    border: 1px solid rgba(66, 153, 225, 0.6);
-  }
-  border: ${(props) =>
-    props.color === "light" ? "solid #0000" : "solid #9a9a9a"};
-  padding: 4px;
-  border-width: var(--b) var(--b) var(--_s) var(--_s);
-  background: conic-gradient(
-        at left var(--d) bottom var(--d),
-        #0000 90deg,
-        rgb(255 255 255 /0.3) 0 225deg,
-        rgb(255 255 255 /0.6) 0
-      )
-      border-box,
-    conic-gradient(at left var(--_s) bottom var(--_s), #0000 90deg, var(--c) 0)
-      0 100% / calc(100% - var(--b)) calc(100% - var(--b)) border-box;
-  transform: translate(calc(var(--d) / -1), var(--d));
-  clip-path: polygon(
-    var(--d) 0%,
-    var(--d) 0%,
-    100% 0%,
-    100% calc(100% - var(--d)),
-    100% calc(100% - var(--d)),
-    var(--d) calc(100% - var(--d))
-  );
-  transition: 0.5s;
-  &:hover {
-    transform: translate(0, 0);
+  display: block;
+  margin-top: 20px;
+  ${media.md} {
+    margin-top: 0;
+    text-align: center;
+    --c: #495049; /* the color */
+    --b: 0.1em; /* border length*/
+    --d: 20px; /* the cube depth */
+    --_s: calc(var(--d) + var(--b));
+    // color: var(--c);
+    &:focus-visible {
+      border: 1px solid rgba(66, 153, 225, 0.6);
+    }
+    border: ${(props) =>
+      props.color === "light" ? "solid #0000" : "solid #9a9a9a"};
+    padding: 4px;
+    border-width: var(--b) var(--b) var(--_s) var(--_s);
+    background: conic-gradient(
+          at left var(--d) bottom var(--d),
+          #0000 90deg,
+          rgb(255 255 255 /0.3) 0 225deg,
+          rgb(255 255 255 /0.6) 0
+        )
+        border-box,
+      conic-gradient(
+          at left var(--_s) bottom var(--_s),
+          #0000 90deg,
+          var(--c) 0
+        )
+        0 100% / calc(100% - var(--b)) calc(100% - var(--b)) border-box;
+    transform: translate(calc(var(--d) / -1), var(--d));
     clip-path: polygon(
-      0% var(--d),
+      var(--d) 0%,
       var(--d) 0%,
       100% 0%,
       100% calc(100% - var(--d)),
-      calc(100% - var(--d)) 100%,
-      0% 100%
+      100% calc(100% - var(--d)),
+      var(--d) calc(100% - var(--d))
     );
-  }
-  span {
-    color: ${(props) => (props.color === "light" ? "#363537" : "#f7f6ee")};
+    transition: 0.5s;
+    &:hover {
+      transform: translate(0, 0);
+      clip-path: polygon(
+        0% var(--d),
+        var(--d) 0%,
+        100% 0%,
+        100% calc(100% - var(--d)),
+        calc(100% - var(--d)) 100%,
+        0% 100%
+      );
+    }
+    // span {
+    //   color: ${(props) => (props.color === "light" ? "#363537" : "#f7f6ee")};
+    // }
   }
 `;
+
+// export const MobileProjectLink = styled.a`
+//   display: block;
+//   ${media.md} {
+//     display: none;
+//   }
+// `;
