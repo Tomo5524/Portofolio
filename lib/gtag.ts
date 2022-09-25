7d65d4b3b2902afee9af44c6ef8976f4cb44262d
Tomo5524 <ttoommoosrst@gmail.com>
2022-09-25 12:39:49 +0900

    fixed build error

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -1,25 +1,25 @@
-// export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
-// // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-// export const pageview = (url: URL) => {
-//   window.gtag("config", GA_TRACKING_ID as string, {
-//     page_path: url,
-//   });
-//   // window && window.gtag && window.gtag('config', GA_TRACKING_ID as string, { page_path: url, })
-// };
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: URL) => {
+  window.gtag("config", GA_TRACKING_ID as string, {
+    page_path: url,
+  });
+  // window && window.gtag && window.gtag('config', GA_TRACKING_ID as string, { page_path: url, })
+};
 
-// interface GTagEvent {
-//   action: string;
-//   category: string;
-//   label: string;
-//   value: number;
-// }
+interface GTagEvent {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+}
 
-// // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-// export const event = ({ action, category, label, value }: GTagEvent) => {
-//   window.gtag("event", action, {
-//     event_category: category,
-//     event_label: label,
-//     value: value,
-//   });
-// };
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: GTagEvent) => {
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
