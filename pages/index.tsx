61086ea44b1ebb5698da588ced1343ac83f4eb5b
Tomo5524 <ttoommoosrst@gmail.com>
2023-12-31 16:49:27 +0900

    fixed meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,38 +24,9 @@ const Home: NextPageWithLayout = () => {
   return (
     <>
       <Head>
-        <title>Tomo Takebuchi</title>
-        <meta name="description" content="Tomo Takebuchi Portfolio" />
+        <title>Tomo Portfolio</title>
+        {/* <meta name="description" content="Tomo Takebuchi Portfolio" /> */}
         {/* <link rel="icon" href="/kitty.ico" /> */}
-        <meta
-          name="viewport"
-          content="initial-scale=1.0, width=device-width"
-        ></meta>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/images/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/images/favicon/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
         {/* preload background image in project page to avoid flickering issue */}
         {/* <link
           rel="preload"
